6b9d1831dbe5c42bc435ec97e98f79bc16fa415d
cerasus1004 <cerasus1004@gmail.com>
2021-11-23 09:57:23 +0200

    js menu

--- markup/static/js/main.js
+++ markup/static/js/main.js
@@ -7,4 +7,11 @@ const header = document.querySelector('header');
 burgerBtn.addEventListener('click', function () {
     header.classList.toggle('header--open');
     navList.classList.toggle('nav-list--close');
+    if (header.classList.contains('header--open')) {
+        burgerBtn.classList.remove('burger-btn--close');
+        burgerBtn.classList.add('burger-btn--open');
+    } else {
+        burgerBtn.classList.remove('burger-btn--open');
+        burgerBtn.classList.add('burger-btn--close');
+    }
 });
